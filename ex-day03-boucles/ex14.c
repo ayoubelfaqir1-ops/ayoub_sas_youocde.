63d57b0284a929a2756bfe6068322c4283abafc5
Ayoub Elfaqir <ayoub.elfaqir@gmail.com>
2025-09-06 14:28:12 +0100

    exercices

--- ex-day03-boucles/ex14.c
+++ ex-day03-boucles/ex14.c
@@ -4,23 +4,28 @@ int main(){
     int n;
     printf("entrez le jour par nmbre(1-Lundi 2-Mardi 3-Mercredi 4-Jeudi 5-Vendredi 6-Samedi 7-Dimanche):");
     scanf("%d",&n);
-
-    for (int i=n-1;i<=6;i++)  {
-
-        printf ("%s\n",jours[i]);
-        
-    }
-    if (n-1!=0)
+    
+    if (n<1||n>7)
     {
-       for (int i = 0; i < n-1; i++)
-       {
-        printf ("%s\n",jours[i]);
-       }
-       
+        printf("entree invalid");
+        return 1;
     }
     
     
     
+    
+    
+    int index=n-1;
+    int i=index;
+    do{
+        printf ("%s\n",jours[i]);
+        i=(i+1)%7;
+    }while (i!=index);
+    
+    
+    
+    
+   
     
     return 0;
 }
